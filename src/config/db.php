7d900a660b553ab4fa2fdc9dc70b00a35a4e72fc
n4mle <namle26022004@gmail.com>
2026-04-06 09:58:15 +0700

    update db config

--- src/config/db.php
+++ src/config/db.php
@@ -4,10 +4,10 @@
  * LMS Admin Panel
  */
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db');
 define('DB_NAME', 'lms_db');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'root');
 define('DB_CHARSET', 'utf8mb4');
 
 try {
